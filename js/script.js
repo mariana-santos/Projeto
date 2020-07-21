289fe379894a8a6f6f241a9b8985bb1d95f4edf1
Mariana Santos <56116824+mariana-santos@users.noreply.github.com>
2020-07-20 23:59:31 -0300

    projeto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var img = document.getElementById('sol1');
+//img.mouseover = function(){ this.src = 'img/sol-hover.jpg'; }
+//img.mouseout = function(){ this.src = 'img/sol1.jpg'; }
+
+/*function hover(element) {
+    element.setAttribute('src', 'img/sol-hover.jpg');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'img/sol1.jpg');
+  }*/
