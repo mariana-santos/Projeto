9bfa869f1dfa4552160843454afbb1b769e11934
mariana-santos <marianasfernandessousa@gmail.com>
2022-01-29 14:55:16 -0300

    :construction: Avançando no popup e checkbox no js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ var img = document.getElementById('sol1');
   function unhover(element) {
     element.setAttribute('src', 'img/sol1.jpg');
   }*/
+
+  var elemento = document.getElementById('conta');
+  var popup = document.getElementById('popup');
+
+  elemento.addEventListener('mouseenter', () => { popup.style.visibility="visible"; });
+  elemento.addEventListener('mouseleave', () => { popup.style.visibility="hidden"; });
+
+  popup.addEventListener('mouseenter', () => { popup.style.visibility="visible"; });
+  popup.addEventListener('mouseleave', () => { popup.style.visibility="hidden"; });
+
+  $('input:radio[name="1"]').change(
+    () => {
+      
+    });
